aaca495f7c19bf8079259e339f244268c20496c0
Tillomaticus <Till.Ikemann@gmail.com>
2024-08-19 10:58:36 +0200

    feat: implemented TTS

--- Assets/Scripts/VoiceSystem.cs
+++ Assets/Scripts/VoiceSystem.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
-using Meta.WitAi.Dictation;
+using Meta.WitAi.TTS.Utilities;
 using Meta.WitAi.CallbackHandlers;
 using UnityEngine.Events;
 using Oculus.Voice;
 using System.Collections;
+using System;
 
 public class VoiceSystem : MonoBehaviour
 {
@@ -14,6 +15,13 @@ public class VoiceSystem : MonoBehaviour
     [SerializeField] private UnityEvent _wakeWordDetected;
 
     [SerializeField] private UnityEvent<string> _completeTranscription;
+
+    [SerializeField] private TTSSpeaker _speaker;
+
+    
+    private string _dateId = "[DATE]";
+
+
 
     private bool _voiceCommandReady;
 
@@ -30,6 +38,7 @@ public class VoiceSystem : MonoBehaviour
         StartCoroutine(ActivateDelayed());
     }
 
+//do this to prevent an error 
     IEnumerator ActivateDelayed()
     {
         yield return new WaitForSeconds(2);
@@ -64,4 +73,53 @@ public class VoiceSystem : MonoBehaviour
         _appVoiceExperience.VoiceEvents.OnPartialTranscription.RemoveListener(OnPartialTranscription);
         _appVoiceExperience.VoiceEvents.OnFullTranscription.RemoveListener(OnFullTranscription);
     }
+
+    public void SpeakText(string text)
+    {
+        text = FormatText(text);
+      /*  if(_speaker.IsSpeaking)
+                 StartCoroutine(SpeakAsync(text, queued));
+        else */
+        _speaker.Speak(text);
+
+    }
+
+   /*  private IEnumerator SpeakAsync(string phrase, bool queued)
+        {
+            // Queue
+            if (queued)
+            {
+                yield return _speaker.SpeakQueuedAsync(new string[] { phrase });
+            }
+            // Default
+            else
+            {
+                yield return _speaker.SpeakAsync(phrase);
+            }
+
+            // Play complete clip
+            if (_asyncClip != null)
+            {
+                _speaker.AudioSource.PlayOneShot(_asyncClip);
+            }
+        } */
+
+
+        // Format text with current datetime
+        private string FormatText(string text)
+        {
+            string result = text;
+            if (result.Contains(_dateId))
+            {
+                DateTime now = DateTime.UtcNow;
+                string dateString = $"{now.ToLongDateString()} at {now.ToLongTimeString()}";
+                result = text.Replace(_dateId, dateString);
+            }
+            return result;
+        }
+
+
+
+    
+
 }
