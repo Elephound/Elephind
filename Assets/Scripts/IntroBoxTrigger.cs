6d70f8f69cf147b1d28185f4c987da528c660259
Tillomaticus <Till.Ikemann@gmail.com>
2024-08-20 11:24:26 +0200

    fix: more various fixes

--- Assets/Scripts/IntroBoxTrigger.cs
+++ Assets/Scripts/IntroBoxTrigger.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class IntroBoxTrigger : MonoBehaviour
 {
    [SerializeField] GameObject introBox;
+    [SerializeField] GameObject chatBox;
+
 
    bool isShown = true;
+   bool firstTime = true;
 
    public void AnyButton()
    {
         isShown = false;
         introBox.SetActive(false);
+        if(firstTime)
+        {
+            firstTime = false;
+            chatBox.SetActive(true);
+        }
    }
 
    public void Reset()
